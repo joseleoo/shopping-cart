db7fb0b4456260d9c5879b03abca386615126e2e
Jose Leonardo Quiñones <56600373+joseleoo@users.noreply.github.com>
2021-05-19 10:12:13 -0500

    export store

--- shopping-cart/store/index.js
+++ shopping-cart/store/index.js
@@ -3,7 +3,7 @@ import Vue from 'vue'
 
 Vue.use(Vuex)
 
-new Vuex.Store({
+export default new Vuex.Store({
     state:{//=data
         products:[]
     },
@@ -19,8 +19,9 @@ new Vuex.Store({
         }
     },
     mutations:{
-        setProducts(){
+        setProducts(state, products	){
             //update products
+            state.products = products
         }
     }
 })
