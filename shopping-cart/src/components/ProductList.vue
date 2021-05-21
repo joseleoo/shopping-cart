9d067bcb1ef01d2aac5f5d9a3e7f135bd4c2afde
Jose Leonardo Quiñones <56600373+joseleoo@users.noreply.github.com>
2021-05-21 17:08:45 -0500

    ended

--- shopping-cart/src/components/ProductList.vue
+++ shopping-cart/src/components/ProductList.vue
@@ -6,14 +6,20 @@
 
     <ul v-else>
       <li v-for="product in products">
-        {{ product.title }} - {{ product.price | currency }} -{{ product.inventory}}
-        <button @click="addProductToCart(product)">Add to Cart</button>
+        {{ product.title }} - {{ product.price | currency }} -{{
+          product.inventory
+        }}
+        <button
+          :disabled="!productIsInStock(product)"
+          @click="addProductToCart(product)"
+        >
+          Add to Cart
+        </button>
       </li>
     </ul>
   </div>
 </template>
 <script>
-
 export default {
   data() {
     return {
@@ -22,8 +28,11 @@ export default {
   },
   computed: {
     products() {
-      return this.$store.getters.availableProducts;
+      return this.$store.state.products;
     },
+    productIsInStock(){
+      return this.$store.getters.productIsInStock
+    }
   },
 
   methods: {
